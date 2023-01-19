9e16c04fa71584896547dd4d04bc7186a63f7554
til2to <padwusup@gmail.com>
2023-01-19 11:39:00 +0000

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,44 @@ import './style.css';
 import sync from './assets/icons/sync.png';
 import downLeft from './assets/icons/down_left.png';
 import { tasks } from './modules/displayTask.js';
+import menuIcon from './assets/icons/menuIcon.png';
 
+const todoContaiter = document.querySelector('.todoContaiter');
 const titleContainer = document.querySelector('.title-list');
 const descriptionContainer = document.querySelector('.description-list');
+
+const tasksArray = [
+  {
+    index: 1,
+    description: 'first task',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'second task',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'third task',
+    completed: false,
+  },
+];
+
+
+tasksArray.forEach((task) => {
+  const taskHtml = `<ul class="task-list">
+  <li class="task-list-item">
+    <input type="checkbox" name="check-task" id="check"/>
+  </li>
+  <li class="task-list-item">${task.description}</li>
+  </li>
+  <li class="task-list-item">
+    <img class="icon" src=${menuIcon} />
+  </li>
+</ul>`;
+  todoContaiter.innerHTML += taskHtml;
+});
 
 const titleHtml = `
   <li class="title-list-item">
@@ -31,5 +66,3 @@ const descriptionHtml = `
   </li>
 `;
 descriptionContainer.innerHTML = descriptionHtml;
-
-tasks();
--- src/modules/displayTask.js
+++ src/modules/displayTask.js
@@ -1,37 +1,37 @@
-import menuIcon from '../assets/icons/menuIcon.png';
+// import menuIcon from '../assets/icons/menuIcon.png';
 
-const todoContaiter = document.querySelector('.todoContaiter');
+// const todoContaiter = document.querySelector('.todoContaiter');
 
-export const tasksArray = [
-  {
-    index: 1,
-    description: 'first task',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'second task',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'third task',
-    completed: false,
-  },
-];
+// export const tasksArray = [
+//   {
+//     index: 1,
+//     description: 'first task',
+//     completed: false,
+//   },
+//   {
+//     index: 2,
+//     description: 'second task',
+//     completed: false,
+//   },
+//   {
+//     index: 3,
+//     description: 'third task',
+//     completed: false,
+//   },
+// ];
 
-export const tasks = () => {
-  tasksArray.forEach((task) => {
-    const taskHtml = `<ul class="task-list">
-    <li class="task-list-item">
-      <input type="checkbox" name="check-task" id="check"/>
-    </li>
-    <li class="task-list-item">${task.description}</li>
-    </li>
-    <li class="task-list-item">
-      <img class="icon" src=${menuIcon} />
-    </li>
-  </ul>`;
-    todoContaiter.innerHTML += taskHtml;
-  });
-};
+// export const tasks = () => {
+//   tasksArray.forEach((task) => {
+//     const taskHtml = `<ul class="task-list">
+//     <li class="task-list-item">
+//       <input type="checkbox" name="check-task" id="check"/>
+//     </li>
+//     <li class="task-list-item">${task.description}</li>
+//     </li>
+//     <li class="task-list-item">
+//       <img class="icon" src=${menuIcon} />
+//     </li>
+//   </ul>`;
+//     todoContaiter.innerHTML += taskHtml;
+//   });
+// };
