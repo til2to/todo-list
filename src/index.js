8c0ad3296d6ec42e3cc8fba3fc136f5931794c1b
til2to <padwusup@gmail.com>
2023-01-19 14:08:54 +0000

    sort todo list based on index before display

--- src/index.js
+++ src/index.js
@@ -10,57 +10,68 @@ const descriptionContainer = document.querySelector('.description-list');
 
 const tasksArray = [
   {
-    index: '',
+    index: '3',
     description: 'first task',
     completed: false,
   },
   {
-    index: '',
+    index: '1',
     description: 'second task',
     completed: false,
   },
   {
-    index: '',
+    index: '2',
     description: 'third task',
     completed: false,
   },
 ];
 
-tasksArray.forEach((task) => {
-  const taskHtml = `<ul class="task-list">
-  <li class="task-list-item">
-    <input type="checkbox" name="check-task" id="check"/>
-  </li>
-  <li class="task-list-item">${task.description}</li>
-  </li>
-  <li class="task-list-item">
-    <img class="icon" src=${menuIcon} />
-  </li>
-</ul>`;
-  todoContaiter.innerHTML += taskHtml;
+// sort by value
+tasksArray.sort(function (a, b) {
+  return a.index - b.index;
 });
 
-const titleHtml = `
-  <li class="title-list-item">
-    <span>Today's To Do</span>
-  </li>
-  <li class="title-list-item">
-    <img class="icon" src=${sync} alt="">
-  </li>
-`;
-titleContainer.innerHTML = titleHtml;
+console.log(tasksArray)
 
-const descriptionHtml = `
-  <li class="description-list-item first-child">
-    <input
-    type="text"
-    name="task-description"
-    id="description"
-    placeholder="Add to your list..."
-    />
-  </li>
-  <li class="description-list-item second-child">
-    <img class="icon" src=${downLeft} />
-  </li>
-`;
-descriptionContainer.innerHTML = descriptionHtml;
+const mainPage = () => {
+  tasksArray.forEach((task) => {
+    const taskHtml = `<ul class="task-list">
+    <li class="task-list-item">
+      <input type="checkbox" name="check-task" id="check"/>
+    </li>
+    <li class="task-list-item">${task.description}</li>
+    </li>
+    <li class="task-list-item">
+      <img class="icon" src=${menuIcon} />
+    </li>
+  </ul>`;
+    todoContaiter.innerHTML += taskHtml;
+  });
+  
+  const titleHtml = `
+    <li class="title-list-item">
+      <span>Today's To Do</span>
+    </li>
+    <li class="title-list-item">
+      <img class="icon" src=${sync} alt="">
+    </li>
+  `;
+  titleContainer.innerHTML = titleHtml;
+  
+  const descriptionHtml = `
+    <li class="description-list-item first-child">
+      <input
+      type="text"
+      name="task-description"
+      id="description"
+      placeholder="Add to your list..."
+      />
+    </li>
+    <li class="description-list-item second-child">
+      <img class="icon" src=${downLeft} />
+    </li>
+  `;
+  descriptionContainer.innerHTML = descriptionHtml;
+}
+
+mainPage()
