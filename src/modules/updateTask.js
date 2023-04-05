4724ca59e1d4241e9b4ec12d7150dcee9c7e34bb
til2to <padwusup@gmail.com>
2023-04-05 08:44:13 +0000

    add function to clear selected task and function to update checkbox

--- src/modules/updateTask.js
+++ src/modules/updateTask.js
@@ -9,4 +9,21 @@ const updateTask = (id, description) => {
   window.localStorage.setItem('tasks', JSON.stringify(getTasks));
 };
 
-export default updateTask;
+// change task's complete status
+const completeTask = (index, checked) => {
+  const tasks = JSON.parse(window.localStorage.getItem('tasks')) || [];
+
+  // Find task with specified index
+  const taskToUpdate = tasks.find((task) => task.index === index);
+  if (!taskToUpdate) {
+    throw new Error(`Task with index ${index} not found`);
+  }
+
+  // Update task checkbox
+  taskToUpdate.completed = checked;
+
+  // Save updated tasks to local storage
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+export { updateTask, completeTask };
